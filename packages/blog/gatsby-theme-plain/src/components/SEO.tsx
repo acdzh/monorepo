2b464a93e371f8b201e9cb0d42f069fdf4b01918
acdzh <acdzh@outlook.com>
2021-09-16 01:28:08 +0800

    feat: update SEO

--- packages/blog/gatsby-theme-plain/src/components/SEO.tsx
+++ packages/blog/gatsby-theme-plain/src/components/SEO.tsx
@@ -2,35 +2,68 @@ import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-type SeoPropsType = {
+import { GraphqlQueryDataType } from '@typings/graphql';
+
+type ArticleInfoType = {
+  publishedTime?: Date | string | number;
+  modifiedTime?: Date | string | number;
+  expirationTime?: Date | string | number;
+  author?: string | string[];
+  section?: string | string[];
+  tag?: string[];
+};
+
+export type SeoPropsType = {
   description?: string;
   lang?: string;
   title?: string;
   image?: string;
   pathname?: string;
   type?: 'website' | 'article';
-  articleInfo?: {
-    publishedTime?: Date | string | number;
-    modifiedTime?: Date | string | number;
-    expirationTime?: Date | string | number;
-    author?: string | string[];
-    section?: string;
-    tag?: string[];
-  };
+  articleInfo?: ArticleInfoType;
   children?: React.ReactNode;
 };
 
-type SeoStaticQueryType = {
-  site: {
-    siteMetadata: {
-      title: string;
-      description: string;
-      siteUrl: string;
-      social: {
-        twitter: string;
-      };
-    };
-  };
+const array2String = (n: string | string[]): string =>
+  Array.isArray(n) ? n.join(',') : n;
+
+const ArticleInfoMetas: React.FC<ArticleInfoType> = ({
+  publishedTime,
+  modifiedTime,
+  expirationTime,
+  author,
+  section,
+  tag,
+}) => {
+  return (
+    <Helmet>
+      {publishedTime && (
+        <meta
+          property="article:published_time"
+          content={new Date(publishedTime).toISOString()}
+        />
+      )}
+      {modifiedTime && (
+        <meta
+          property="article:modified_time"
+          content={new Date(modifiedTime).toISOString()}
+        />
+      )}
+      {expirationTime && (
+        <meta
+          property="article:expiration_time"
+          content={new Date(expirationTime).toISOString()}
+        />
+      )}
+      {author && (
+        <meta property="article:author" content={array2String(author)} />
+      )}
+      {section && (
+        <meta property="article:section" content={array2String(section)} />
+      )}
+      {tag && <meta property="article:tag" content={array2String(tag)} />}
+    </Helmet>
+  );
 };
 
 export const SEO: React.FC<SeoPropsType> = ({
@@ -43,7 +76,7 @@ export const SEO: React.FC<SeoPropsType> = ({
   pathname = '/',
   children,
 }) => {
-  const { site } = useStaticQuery<SeoStaticQueryType>(
+  const { site } = useStaticQuery<GraphqlQueryDataType>(
     graphql`
       query {
         site {
@@ -65,75 +98,53 @@ export const SEO: React.FC<SeoPropsType> = ({
   const url = `${site.siteMetadata.siteUrl}${pathname}`.replace(/\/\/$/, '/');
 
   return (
-    <Helmet defaultTitle={defaultTitle} titleTemplate={`%s | ${defaultTitle}`}>
-      {/* https://devhints.io/html-meta */}
-      <html lang={lang} />
-      <meta charSet="utf-8" />
+    <>
+      <Helmet
+        defaultTitle={defaultTitle}
+        titleTemplate={`%s | ${defaultTitle}`}
+      >
+        {/* https://devhints.io/html-meta */}
+        <html lang={lang} />
+        <meta charSet="utf-8" />
 
-      {/* title */}
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
+        {/* title */}
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="twitter:title" content={title} />
 
-      {/* url */}
-      <link rel="canonical" href={url} />
-      <meta property="og:url" content={url} />
-      <meta name="twitter:url" content={url} />
+        {/* url */}
+        <link rel="canonical" href={url} />
+        <meta property="og:url" content={url} />
+        <meta name="twitter:url" content={url} />
 
-      {/* description */}
-      <meta name="description" content={metaDescription} />
-      <meta property="og:description" content={metaDescription} />
-      <meta name="twitter:description" content={metaDescription} />
+        {/* description */}
+        <meta name="description" content={metaDescription} />
+        <meta property="og:description" content={metaDescription} />
+        <meta name="twitter:description" content={metaDescription} />
 
-      {/* image */}
-      {image && (
-        <>
-          <meta property="og:image" content={image} />
-          <meta name="twitter:image" content={image} />
-        </>
-      )}
+        {/* image */}
+        {image && (
+          <>
+            <meta property="og:image" content={image} />
+            <meta name="twitter:image" content={image} />
+          </>
+        )}
 
-      {/* ua */}
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        {/* ua */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-      {/* more information */}
-      <meta property="og:site_name" content={title} />
-      <meta property="og:type" content={type} />
+        {/* more information */}
+        <meta property="og:site_name" content={title} />
+        <meta property="og:type" content={type} />
 
+        {/* Apple Only */}
+        <meta name="format-detection" content="telephone=no" />
+
+        {children}
+      </Helmet>
       {type === 'article' && articleInfo && (
-        <>
-          {articleInfo.publishedTime && (
-            <meta
-              property="article:published_time"
-              content={new Date(articleInfo.publishedTime).toISOString()}
-            />
-          )}
-          {articleInfo.modifiedTime && (
-            <meta
-              property="article:modified_time"
-              content={new Date(articleInfo.modifiedTime).toISOString()}
-            />
-          )}
-          {articleInfo.expirationTime && (
-            <meta
-              property="article:expiration_time"
-              content={new Date(articleInfo.expirationTime).toISOString()}
-            />
-          )}
-          {articleInfo.author && (
-            <meta property="article:author" content={type} />
-          )}
-          {articleInfo.section && (
-            <meta property="article:section" content={type} />
-          )}
-          {articleInfo.tag && <meta property="article:tag" content={type} />}
-        </>
+        <ArticleInfoMetas {...articleInfo} />
       )}
-
-      {/* Apple Only */}
-      <meta name="format-detection" content="telephone=no" />
-
-      {children}
-    </Helmet>
+    </>
   );
 };
