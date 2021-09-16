fe42380ac30d5733f67f96f5e018bd85ce7fb4aa
acdzh <acdzh@outlook.com>
2021-09-16 18:06:19 +0800

    feat: add site manifest

--- packages/blog/gatsby-blog-src/gatsby-config.js
+++ packages/blog/gatsby-blog-src/gatsby-config.js
@@ -18,6 +18,21 @@ module.exports = {
     'gatsby-plugin-pnpm',
     'gatsby-theme-plain',
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: '随便写写',
+        short_name: '随便写写',
+        description: '随便写写, 全而不精',
+        lang: 'zh-CN',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: ' #fff',
+        display: 'standalone',
+        icon: 'assets/favicon.png',
+        theme_color_in_head: false, // This will avoid adding theme-color meta tag.
+      },
+    },
+    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-148601605-2',
