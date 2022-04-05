2b4767782d7d093500e1fca375d3a68ba6181f06
acdzh <acdzh@outlook.com>
2022-04-06 02:39:22 +0800

    feat: 新分类统计

---
+++ packages/apps/shanghai-covid/src/pages/Dot/components/index.ts
@@ -0,0 +1,4 @@
+export * from './按地址统计Modal';
+export * from './按日期统计Modal';
+export * from './分地区统计Modal';
+export * from './所有数据Modal';
---
+++ packages/apps/shanghai-covid/src/pages/Dot/index.tsx
@@ -0,0 +1,142 @@
+import { DotMap } from '@ant-design/maps';
+import { Button, DatePicker, Space } from 'antd';
+import type { Moment } from 'moment';
+import React, { useState } from 'react';
+
+import { useLocation } from '../../hooks';
+import { PatientType, patients } from '../../libs';
+
+import {
+  分地区统计Modal,
+  所有数据Modal,
+  按地址统计Modal,
+  按日期统计Modal,
+} from './components';
+import { datePickerRanges, mapConfig } from './constants';
+import { 根据日期筛选数据, 统计数量并添加扰动 } from './utils';
+
+const { RangePicker } = DatePicker;
+
+const 初始数据 = patients;
+const 扰动后的初始数据 = 统计数量并添加扰动(初始数据);
+
+const Dot: React.FC = () => {
+  const [is分地区统计ModalVisible, setIs分地区统计ModalVisible] =
+    useState(false);
+  const [is按日期统计ModalVisible, setIs按日期统计ModalVisible] =
+    useState(false);
+  const [is按地址统计ModalVisible, setIs按地址统计ModalVisible] =
+    useState(false);
+  const [is所有数据ModalVisible, setIs所有数据ModalVisible] = useState(false);
+
+  const [location] = useLocation();
+  const [currentPatients, setCurrentPatients] =
+    useState<PatientType[]>(扰动后的初始数据);
+
+  const onRangeChange = (dates: [Moment | null, Moment | null] | null) => {
+    const 根据日期筛选后的数据 = 根据日期筛选数据(patients, dates);
+    console.log(根据日期筛选后的数据);
+
+    const 扰动后的数据 = 统计数量并添加扰动(根据日期筛选后的数据);
+    setCurrentPatients(扰动后的数据);
+  };
+
+  return (
+    <>
+      <DotMap
+        {...mapConfig}
+        map={{
+          type: 'amap',
+          center: location,
+          pitch: 15,
+          style: 'dark',
+          zoom: 10,
+        }}
+        source={{
+          data: currentPatients,
+          parser: {
+            type: 'json',
+            x: 'lng',
+            y: 'lat',
+          },
+        }}
+      />
+      <div
+        style={{
+          position: 'fixed',
+          zIndex: 900,
+          top: '10px',
+          left: '10px',
+          right: '10px',
+        }}
+      >
+        <div>
+          <RangePicker
+            onChange={onRangeChange}
+            ranges={datePickerRanges}
+            style={{
+              height: '30px',
+              marginRight: '48px',
+            }}
+          />
+        </div>
+        <Space style={{ marginTop: '8px' }}>
+          <Button
+            type="primary"
+            size="small"
+            ghost
+            onClick={() => setIs分地区统计ModalVisible(true)}
+          >
+            分地区统计
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            ghost
+            onClick={() => setIs按日期统计ModalVisible(true)}
+          >
+            按日期统计
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            ghost
+            onClick={() => setIs按地址统计ModalVisible(true)}
+          >
+            按地址统计
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            ghost
+            onClick={() => setIs所有数据ModalVisible(true)}
+          >
+            所有数据
+          </Button>
+        </Space>
+      </div>
+      <分地区统计Modal
+        patients={currentPatients}
+        visible={is分地区统计ModalVisible}
+        onClose={() => setIs分地区统计ModalVisible(false)}
+      />
+      <按日期统计Modal
+        patients={currentPatients}
+        visible={is按日期统计ModalVisible}
+        onClose={() => setIs按日期统计ModalVisible(false)}
+      />
+      <按地址统计Modal
+        patients={currentPatients}
+        visible={is按地址统计ModalVisible}
+        onClose={() => setIs按地址统计ModalVisible(false)}
+      />
+      <所有数据Modal
+        patients={currentPatients}
+        visible={is所有数据ModalVisible}
+        onClose={() => setIs所有数据ModalVisible(false)}
+      />
+    </>
+  );
+};
+
+export default Dot;
