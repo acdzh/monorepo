7858514c8e794445456c8eb98a3db8a5cfe1d4f5
acdzh <acdzh@outlook.com>
2022-04-30 20:43:30 +0800

    feat: loading

--- packages/apps/shanghai-covid/src/pages/Dot/components/关于Modal.tsx
+++ packages/apps/shanghai-covid/src/pages/Dot/components/关于Modal.tsx
@@ -9,9 +9,7 @@ export const 关于Modal: React.FC<{
 }> = ({ visible, onClose }) => (
   <Modal title="关于" visible={visible} onOk={onClose} onCancel={onClose}>
     <Paragraph>
-      本站不是官方网站, 仅用于交流与学习, 一切信息请以<b>官方口径</b>为准.
-    </Paragraph>
-    <Paragraph>
+      本站不是官方网站, 仅用于交流与学习, 一切信息请以<b>官方</b>为准.
       所有数据来自
       <Link
         href="https://wsjkw.sh.gov.cn/xwfb/index.html"
@@ -20,21 +18,22 @@ export const 关于Modal: React.FC<{
       >
         上海市卫建委
       </Link>
-      公布的疫情相关<b>地点</b>的数据, 不是确诊或无症状病例数据,
-      因为每天同一地点的病例在通报中会合并, 所以地址数量会
-      <b>远小于</b>病例数量.
+      公布的疫情相关<b>地点</b>, 不是确诊或无症状病例数据.
+      每天同一地点的病例在通报中会合并, 因此地址数量
+      <b>不等于</b>病例数量.
     </Paragraph>
     <Paragraph>
-      数据每天中午定时更新, 地理坐标为自动生成, 存在误差或偏移等现象,
-      属于正常现象. 如果想<b>查询具体的地址是否存在相关风险</b>, 请点击
-      <code>所有数据</code>
-      按钮, 之后使用
-      <code>地址</code>一栏的筛选按钮进行搜索.
+      数据每天定时更新, 地理坐标为自动生成, 存在误差或偏移.{' '}
+      <b>要查询具体的地址是否存在相关风险</b>, 请点击
+      <code>所有数据</code>, 之后使用
+      <code>地址</code>栏的筛选按钮进行搜索.
     </Paragraph>
     <Paragraph>
-      本站不保证数据的及时性, 正确性与完整性. 因数据量激增, 页面<b>有点卡</b>(
-      <del>又不是不能用.jpg</del>), 请耐心等待加载, 数据量较大, 所以默认只展示
-      <b>前七天数据</b>, 可以自行选择时间范围. 如有问题, <b>请联系开发者</b>:{' '}
+      本站不保证数据的及时性, 正确性与完整性. 数据量较大, 请耐心等待加载. 页面
+      <b>默认只展示前七天</b>(包括统计弹窗中的数据), 可以自行选择时间范围.
+    </Paragraph>{' '}
+    <Paragraph>
+      如有问题, 请联系开发者:{' '}
       <Link
         href="https://www.github.com/acdzh"
         target="_blank"
@@ -50,17 +49,8 @@ export const 关于Modal: React.FC<{
       >
         email
       </Link>
-      . 工作繁忙, 非 Bug 类型反馈更新不会及时.
+      . 工作繁忙, 非 Bug 类型反馈不会及时处理.
     </Paragraph>
-    <Paragraph>
-      <Link
-        href="https://www.zhihu.com/question/368570142/answer/1002837086"
-        target="_blank"
-      >
-        疫情会放大焦虑等负面情绪
-      </Link>
-      , 希望各位能够调整好心态, 共克时艰, 谢谢.
-    </Paragraph>
-    <Paragraph>上一次功能更新于: 2022 年 4 月 30 日.</Paragraph>
+    <Paragraph>上一次功能更新于 2022 年 4 月 30 日.</Paragraph>
   </Modal>
 );
