4ee41c11ca11106ce7ca049c3214015aa27843af
acdzh <acdzh@outlook.com>
2022-05-04 17:16:59 +0800

    feat: update vf font

--- packages/blog/gatsby-theme-plain/windi.config.ts
+++ packages/blog/gatsby-theme-plain/windi.config.ts
@@ -44,6 +44,7 @@ export default defineConfig({
         ],
         serif: ['Georgia', 'serif'],
         mono: [
+          'Fira Code VF',
           'Fira Code',
           'Monaco',
           'Menlo',
