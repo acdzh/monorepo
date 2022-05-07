3f78f848edb44eb33ed88a65cf0532837f5fc142
acdzh <acdzh@outlook.com>
2022-05-08 02:44:08 +0800

    feat: image

--- packages/blog/gatsby-theme-plain/src/components/post/MDXComponents.tsx
+++ packages/blog/gatsby-theme-plain/src/components/post/MDXComponents.tsx
@@ -8,13 +8,7 @@ import { bilibili, codepen, netease, youtube } from './shortcodes';
 export const MDXComponents: MDXProviderComponentsProp = {
   code: Codeblock,
   pre: (props) => <>{props.children}</>,
-  img: (props: React.HTMLAttributes<HTMLImageElement>) => (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <img {...props} className={clsx(props.className, 'max-w-650px m-auto')} />
-  ),
-  figcaption: (props: React.HTMLAttributes<HTMLElement>) => (
-    <figcaption {...props} className={clsx(props.className, 'text-center')} />
-  ),
+  del: (props) => <del title="被你发现了..." {...props} />,
   bilibili,
   codepen,
   netease,
